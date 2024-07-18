02c57d7939aac44254c3ab1734599521a2a7bef0
4Ts-code <eo8407272@gmail.com>
2024-07-18 02:05:53 +0100

    Tutorials

---
+++ Tutorials/SomeCodes/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    do {
+        cout << "Hello World"<<endl;
+    }
+    while (2 < 5);
+    cout << "Hello world!" << endl;
+    return 0;
+}
